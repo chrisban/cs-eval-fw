2086b23ebeeb74be76a3305461ce677e836e8d9c
Christopher Ban <chrisban@me.com>
2017-01-25 10:03:45 -0800

    Admin page authorization

--- server.js
+++ server.js
@@ -2,62 +2,41 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var busboy = require('connect-busboy');
+var auth = require('http-auth');
+var basic = auth.basic({
+    realm: "Admin",
+    file: __dirname + "/admin/users.htpasswd"
+});
 
 var functions = require('./includes/js/serverFunctions');
 
 // for parsing application/json and files
-app.use(express.cookieParser());
-app.use(express.session({secret: '1234567890QWERTY'}));
 app.use(bodyParser.json());
-app.use(app.router);
 app.use(busboy());
+app.use(auth.connect(basic));
 
 //connect static links
 app.use('/includes', express.static(__dirname + '/includes'));
 app.use('/admin', express.static(__dirname + '/admin'));
 
-var USER = "admin";
-var PASS = "pass";
-
-function requireLogin(req, res, next) {
-	console.log('req', req.body);
-  if (req.session == 'undefined' || req.session !== null || !req.session.loggedIn) {
-  	console.log("not loggedin");
-    // require the user to log in
-    res.sendFile(  __dirname + "/admin/auth.html" );
-  } else {
-  	console.log("df");	
-  	next(); // allow the next route to run
-  }
-}
-
 
 /*********************/
 /* HTTP GET HANDLING */
 /*********************/
-// Automatically apply the `requireLogin` middleware to all
-// routes starting with `/admin`
-app.all("/admin/*", requireLogin, function(req, res, next) {
-  next(); // if the middleware allowed us to get here,
-          // just move on to the next route handler
-});
-
 app.get('/logout', function (req, res) {
   //delete req.session.user;
   res.redirect('/admin');
 });  
 
 //Admin page
-app.get('/admin/', function (req, res) {
- 	requireLogin(req, res, res.sendFile( __dirname + "/admin/admin.html" ))	;
+app.get('/admin', auth.connect(basic), (req, res) => {
+    res.sendFile(  __dirname + "/admin/admin.html" );
 });
 
 //GET file manager tree data (json)
 app.get('/getTree', function (req, res) {
 	functions.getTree('./dataFiles', res)
 });
-
-
 		
 //Exam/Quiz module
 app.get('/', function (req, res) {
